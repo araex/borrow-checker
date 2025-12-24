e64eba030bf5e21ccec986947c0489dc1bb68f6b
Araex <3930554+araex@users.noreply.github.com>
2025-12-24 13:16:33 +0100

    add local storage use example

--- src/components/counter_btn.rs
+++ src/components/counter_btn.rs
@@ -1,15 +1,18 @@
+use codee::string::FromToStringCodec;
 use leptos::prelude::*;
+use leptos_use::storage::*;
 
 /// A parameterized incrementing button
 #[component]
 pub fn Button(#[prop(default = 1)] increment: i32) -> impl IntoView {
-    let (count, set_count) = signal(0);
+    // let (count, set_count) = signal(0);
+    let (count, set_count, _) = use_local_storage::<i32, FromToStringCodec>("cnt");
     view! {
         <button on:click=move |_| {
             set_count.set(count.get() + increment)
         }>
 
-            "Click me: " {count}
+            "Add " {increment} ": " {count}
         </button>
     }
 }
